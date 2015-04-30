3d8c00bf787f9c38045e5906dd85eab1ecd65d49
Mubhij Ahmad <howlbay@gmail.com>
2015-04-30 18:16:42 -0500

    done

--- samplecode/googlemaps.js
+++ samplecode/googlemaps.js
@@ -15,13 +15,14 @@ var map;
         
         //waits for google maps to load knockout.This is not ideal for a website but is fine for a game.
        google.maps.event.addListenerOnce(map, 'idle', function(){
-            ko.applyBindings(tabview,$("#maincontainer")[0]);
+            ko.applyBindings(hungry,$("#maincontainer")[0]);
+            alert('cool');
         });
         
         return map;
       }
       
-      
+      initialize();
       
       //only for one address. It returns a latlng object.
       function geocode(address,success,error)
@@ -32,7 +33,7 @@ var map;
         if (status==google.maps.GeocoderStatus.OK)
         {
             var coods=response[0].geometry.location;
-            success(coods,region);
+            success(coods);
         }else{
             error();
         }
@@ -40,8 +41,11 @@ var map;
       }
       
       //gets distance in meters between two objects. It assumes a flat earth as we are dealing with relatively short distances where arcs are not that important.
+      //takes in googlemaps latlng objects.
       function distance(object1,object2){
-        
+        var d=Math.sqrt(Math.pow(object2.lat()-object1.lat(),2)+Math.pow(object2.lng()-object1.lng()),2);
+        var geotometer=111000;
+        return geotometer*d;
       }
       
       //object for google places. This contains functions to search for attractions and cleans them up.
@@ -59,7 +63,7 @@ var map;
                     if (status==google.maps.places.PlacesServiceStatus.OK) {
                         //this api returns in the form array(0=>array(details))
                         
-                        fn(response)
+                        fn(response);
                         
                     }
                 });
@@ -86,15 +90,18 @@ var map;
             
           }
         
+        //cleans up the google maps response.
             self.attractionscleanup=function(response){
-            var ret=[];
-            for (var i=0;i<response.length;i++) {
-                ret[i]={};
-                ret[i].address=response[i]['formatted_address'];
-                ret[i].geocode=response[i].geometry.location;
-                ret[i].rating=response[i].rating;
-                ret[i].name=response[i].name;
-                
+                var ret=[];
+                for (var i=0;i<response.length;i++) {
+                    ret[i]={};
+                    ret[i].address=response[i]['formatted_address'];
+                    ret[i].geocode=response[i].geometry.location;
+                    ret[i].rating=response[i].rating;
+                    ret[i].name=response[i].name;
+                    
+                }
+                return ret;
             }
       }
       
--- samplecode/hungry.js
+++ samplecode/hungry.js
@@ -1,4 +1,4 @@
-//this code use the google maps api to geocode an input location, map it, find 10 prominent restaurants near it and find the shortest route to each restaurant.
+//this code use the google maps api to geocode an input location, map it, find 10 prominent restaurants near it and find the shortest route to each sequential restaurant.
 //each visit to a restaurant bears a probability equal to its rating that you will get sick and will exit the game. Find the best location to be in while hungry.
 
 //knockout model. Knockout is great for callback functions and would make life easier for google maps api.
@@ -10,6 +10,7 @@ function HungryModel() {
     self.usergeocode=ko.observable(null);
     self.errors=ko.observable(null);
     self.attractions=ko.observable(null);
+    self.gplaces= new gplaces();
     
     
     //creates an autocomplete feature to make it easy for the user to enter a location.
@@ -25,6 +26,8 @@ function HungryModel() {
      self.SearchSubmit=function(){
         //checks if userlocation has been filled in. Else returns an error.
         if (self.userlocation()) {
+            self.usergeocode(null);
+            self.attractions(null);
             geocode(self.userlocation(),function(coods){
                 self.usergeocode(coods);
             },function(){
@@ -39,7 +42,7 @@ function HungryModel() {
      //this finds restaurants nearby a 20 mile radius based on the usergeocode whenever the usergeocode changes.
      self.getmefood=ko.computed(function(){
         if (self.usergeocode()) {
-            gplaces.attractionsearch({
+            self.gplaces.attractionsearch({
                 radius:20*1609,
                 geocode: self.usergeocode()
             },function(response){
@@ -50,8 +53,40 @@ function HungryModel() {
      
      //finds the order of attractions to visit in whenever self.attractions changes
      self.findattractionorder=ko.computed(function(){
-        if (self.attractions()) {
+        if (self.attractions() && self.usergeocode()) {
+            //order keeps a list of indices for self.attractions in the order they are to be visited.
+            var order=[];
+            var distances=self.attractions();
+            //get distances of each object from user.
+            for(var i=0; i<self.attractions().length;i++){
+                distances[i]={distance:distance(self.usergeocode(),self.attractions()[i].geocode), index: i};
+            }
+            distances.sort(function(a,b){
+                return parseFloat(a.distance)-parseFloat(b.distance);
+            });
             
+            order[0]=distances[0].index;
+            distances.splice(0,1);
+            var orderindex=0;
+            
+            
+            //this finds the shortest point from each point while destroying the used up points.
+            while(distances.length>0){
+                var shortest=-1;
+                var tmp;
+                
+                for(var i=0; i<distances.length; i++){
+                    var dist=distance(self.attractions()[order[orderindex]].geocode,self.attractions()[distances[i].index].geocode)
+                    if (shortest==-1 || parseFloat(dist)<parseFloat(shortest)) {
+                        shortest=dist;
+                        tmp=i;
+                    }
+                }
+                order[Number(orderindex)+1]=distances[tmp].index;
+                distances.splice(tmp,1);
+                orderindex++;
+            }
+            alert(JSON.stringify(order));
         }
      });
      
